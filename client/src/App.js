e90efb76cfbbdf76f215f031701c84db2399d014
Jonathan Mohabir <jonamohabir416@gmail.com>
2023-03-07 10:05:04 -0500

    FRONT END UPDATE - Fixed first landing page InvestMoney.js (centered content and fully reflects UI design from Figma), move to other pages and do the same styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 
+
 import { Routes, Route } from 'react-router-dom';
 // Children of BrowserRouter -- index.js conatains parent.
 
--- client/src/components/landingcomponents/InvestMoney.js
+++ client/src/components/landingcomponents/InvestMoney.js
@@ -1,12 +1,10 @@
 import React from 'react';
-
 import LandingImage01 from './images/LandingImage01.svg';
 
 function InvestMoney() {
   return (
-    // let all components respond to the screen size, have content centered oriented
-    <div className="container ">
-      <section>
+    <div className="d-flex flex-column justify-content-center align-items-center vh-100">
+      <section id="image-section" className="text-center mb-5">
         <img
           src={LandingImage01}
           alt="Watering Money Tree"
@@ -14,22 +12,30 @@ function InvestMoney() {
         ></img>
       </section>
 
-      <section>
+      <section id="header-section" className="text-center mb-3">
         <header>
-          <h2>Invest Your Money</h2>
+          <h2 className="mb-0">Invest Your Money</h2>
         </header>
       </section>
-      <section>
+
+      <section id="text-section" className="text-center mb-5">
         <p>
-          Get accesss to risk free investments that will multiply your income
-          and pay high returns in few months
+          Get access to risk-free investments that will multiply your income and
+          pay high returns in a few months
         </p>
       </section>
 
-      <section className="buttons-section">
-        <button className="btn btn-primary">Create Your Account</button>
-        <br></br>
-        <button className='btn btn-primary"'> Login into your account</button>
+      <section
+        id="button-section"
+        className="buttons-section d-flex flex-column align-items-center"
+      >
+        <button className="btn btn-primary mb-3">Create Your Account</button>
+        <button
+          className="btn btn-primary"
+          style={{ backgroundColor: 'white' , color: 'black'}}
+        >
+          Login into your account
+        </button>
       </section>
     </div>
   );
